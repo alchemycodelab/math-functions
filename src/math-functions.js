adf3c8826c718ec9309c1531356e9951332291ed
sanvean74 <dlambertpdx@gmail.com>
2019-07-19 14:50:24 -0700

    fixed sum function

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,10 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const added = a + b;
+    const message = 'The sum of ' + a + ' and ' + b + ' is ' + added + '.';
+    const array = [added, message];
+    return array;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
