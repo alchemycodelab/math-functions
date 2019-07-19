864c99d1e91759921270c0e0ece56cb9cabd0caf
allisonbusse <allisonbusse@gmail.com>
2019-07-19 14:43:32 -0700

    completed sum function

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,9 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const total = a + b;
+    const totalStatement = 'The sum of ' + a + ' and ' + b + ' is ' + total + '.';
+    return [total, totalStatement];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
