e4947d8f029e11af2958beb33cc98854939d95cf
Angela Vawser <amvawser@gmail.com>
2019-07-19 15:56:40 -0700

    completed multiply array with 3 function

--- src/math-functions.js
+++ src/math-functions.js
@@ -109,7 +109,10 @@ you may continue to use the + operator for string concatenation.
 */
 
 export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
-
+    const multArrayOne = multiply(multArr[0], multArr[1])[0];
+    const multArrayTwo = multiply(multArrayOne, multArr[2])[0];
+    const multArrayMsg = 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + multArrayTwo + '.';
+    return [multArrayTwo, multArrayMsg];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
