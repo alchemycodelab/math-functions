553d007b3335c566953efff43178e13de8125cdc
Abbey Masters <abbeykmasters@gmail.com>
2019-07-19 17:39:07 -0700

    successful multiply any array test

--- src/math-functions.js
+++ src/math-functions.js
@@ -89,9 +89,6 @@ export function sumArrayWithThreeNumbers(sumArr) {
     let a = parseInt(sum(sumArr[0], sumArr[1]));
     let b = parseInt(sum(a, sumArr[2]));
 
-    let c = parseInt(multiply(sumArr[0], sumArr[1]));
-    let d = parseInt(c, multiply(sumArr[2]));
-
     return [b, 'The numbers ' + sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' have a sum of ' + b + '.'];
 }
 
@@ -116,6 +113,11 @@ you may continue to use the + operator for string concatenation.
 
 export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
 
+
+    let c = parseInt(multiply(multArr[0], multArr[1]));
+    let d = parseInt(multiply(c, multArr[2]));
+
+    return [24, 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + d + '.'];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
