5e1b915d81f672c5d7a0e0b9834363385d9c3386
sethripman <sethripman@gmail.com>
2019-10-07 09:38:51 -0700

    wrote multiply function

--- src/math-functions.js
+++ src/math-functions.js
@@ -30,6 +30,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
+    const total = a * b;
+    const sumString = `The product of ${a} and ${b} is ${total}.`;
+    const sumArray = [total, sumString];
+
+    return sumArray;
 
 }
 
