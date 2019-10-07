7ed3951e8f12254436fa5c23f5a18d0052438dcc
Nick <nickbrimmer@gmail.com>
2019-10-07 10:15:16 -0700

    this is the second math problem

--- src/math-functions.js
+++ src/math-functions.js
@@ -11,8 +11,6 @@ example and uses the values that were input into the function:
 "The sum of 4 and 7 is 11."
 */
 
-
-
 export function sum(a, b) {
     const array = [];
     array [0] = a + b;
@@ -33,7 +31,10 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    const array = [];
+    array [0] = a * b;
+    array [1] = `The product of ${a} and ${b} is ${array[0]}.`;
+    return array;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
