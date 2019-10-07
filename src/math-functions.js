1434d43dba1dfd4e062d5ff1dbdbf554df967d2b
Matt Munch <mattmunch94@gmail.com>
2019-10-07 10:03:17 -0700

    problem three complete

--- src/math-functions.js
+++ src/math-functions.js
@@ -52,7 +52,9 @@ how to do this. However, you may continue to use the + operator for string conca
 */
 
 export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
-
+    let sumTotal = sum(sum(a, b)[0], c)[0];
+    let productTotal = multiply(multiply(a, b)[0], c)[0];
+    return [sumTotal, productTotal, a + ' and ' + b + ' and ' + c + ' sum to ' + sumTotal + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productTotal + '.'];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
