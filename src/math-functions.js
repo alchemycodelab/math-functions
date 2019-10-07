bf28832b7a2d95eefb983fb0d18488c907060fce
Eve Stockstill <evestockstillpdx@gmail.com>
2019-10-06 18:26:00 -0700

    sumArry

--- src/math-functions.js
+++ src/math-functions.js
@@ -75,7 +75,8 @@ to use the + operator for string concatenation.
 */
 
 export function sumArrayWithThreeNumbers(sumArr) {
-
+    const sums = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+    return [sums, `The numbers ${sumArr.join()} have a sum of ${sums}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
