f071001fa517603a648aeaf481d8b4fe22a75e3b
Nick <nickbrimmer@gmail.com>
2019-10-07 09:52:24 -0700

    this is my message

--- src/math-functions.js
+++ src/math-functions.js
@@ -11,9 +11,16 @@ example and uses the values that were input into the function:
 "The sum of 4 and 7 is 11."
 */
 
+
+
 export function sum(a, b) {
+    const array = [];
+    array [0] = a + b;
+    array [1] = `The sum of ${a} and ${b} is ${array[0]}.`;
+    return array;    
 
 }
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
