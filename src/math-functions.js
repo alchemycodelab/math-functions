c7194aace7bac19b7664c9f92fb5eb75cc85a178
TravelFiend <mikegrace274@gmail.com>
2019-10-07 10:23:29 -0700

    stretch goal added

--- src/math-functions.js
+++ src/math-functions.js
@@ -122,7 +122,11 @@ This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-    
+    let prodOfArr = 1;
+    for(let i = 0; i < dynamicArray.length; i++){
+        prodOfArr = multiply(prodOfArr, dynamicArray[i])[0];
+    }
+    return [prodOfArr, `The numbers ${dynamicArray.join()} have a product of ${prodOfArr}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
