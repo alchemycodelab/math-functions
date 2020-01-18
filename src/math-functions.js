bc7440b07ccf3a631640ccf0449eda78c5bcd7fb
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-18 13:19:25 -0800

    solve problem 2 and make a correction to problem 1 solution

--- src/math-functions.js
+++ src/math-functions.js
@@ -13,7 +13,7 @@ example and uses the values that were input into the function:
 
 export function sum(a, b) {
     const result = (a + b)
-    const array = [result, `The sum of ${a} and ${b} is 11.`];
+    const array = [result, `The sum of ${a} and ${b} is ${result}.`];
     return array;
 }
 
@@ -30,8 +30,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    const product = a * b;
+    return [product, `The product of ${a} and ${b} is ${product}.`];
 }
+
+multiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
