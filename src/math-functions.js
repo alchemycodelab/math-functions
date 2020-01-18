7e37c7a3ae80ae6967373ed0e39e22e0d68fb8c1
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-18 13:09:46 -0800

    solve problem 1

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,8 +12,12 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const result = (a + b)
+    const array = [result, `The sum of ${a} and ${b} is 11.`];
+    return array;
 }
+
+sum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
