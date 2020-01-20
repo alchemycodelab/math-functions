df6eae93131dba2d6c1f6d5e1fc4263126aba670
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-19 16:35:06 -0800

    fix second problem

--- src/math-functions.js
+++ src/math-functions.js
@@ -29,6 +29,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
+    const total = a * b;
+    const result = [total, 'The product of ' + a + ' and ' + b + ' is ' + total + '.'];
+    return result;
 
 }
 
