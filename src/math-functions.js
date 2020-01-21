66083bc6e9fb7e58a46b97fe1bbbf810ad87b922
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-21 09:56:22 -0800

    create multAnyArray

--- src/math-functions.js
+++ src/math-functions.js
@@ -155,8 +155,15 @@ This function should be dynamic, accepting an array of any length.
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
+    let num = 1;
 
+    for(let i = 0; i < dynamicArray.length; i++) {
+        num = multiply(dynamicArray[i], num)[0];        
+    }
+    
+    return [num, `The numbers ${dynamicArray} have a product of ${num}.`];
 }
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // You're done! Submit the link to the repo following the instructions in Canvas.
