9f36b31602183b4504043109f4dc98f786e40449
dorjepadma <dorjepadma@gmail.com>
2020-01-21 12:03:00 -0800

    problem 5 done

--- src/math-functions.js
+++ src/math-functions.js
@@ -111,7 +111,10 @@ you may continue to use the + operator for string concatenation.
 */
 
 export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
-
+    const times = multiply(multiply (multArr[0], multArr[1])[0], multArr[2])[0];
+    let multipleArray = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${times}.`;
+    return [times, multipleArray];
+   
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
