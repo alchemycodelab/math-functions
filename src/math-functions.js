3b64e52f132161789e86bf97c3959893294277e1
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-01-21 10:55:47 -0800

    solved for problem 5

--- src/math-functions.js
+++ src/math-functions.js
@@ -112,22 +112,36 @@ you may continue to use the + operator for string concatenation.
 */
 
 export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
-
+    const a = multArr[0];
+    const b = multArr[1];
+    const c = multArr[2];
+    const productOfArr = multiply(a, b)[0];
+    const finalProductArr = multiply(c, productOfArr)[0];
+    const arrayFive = [finalProductArr, `The numbers ${multArr} have a product of ${finalProductArr}.`];
+    return arrayFive;
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between 
+//GitHub and your laptop. 
 
-// You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
+// You're done! Submit the link to the repo following the instructions in Canvas. Or, 
+// try out the stretch goal below...
 
 // Don't forget to create a new branch for your work on the next question, if you attempt it.
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as 
+its argument and returns an array whose first element is the product of those numbers, and 
+the second element is a string that EXACTLY follows this example and concatenates a message 
+using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do 
+multiplication, use your multiply() function that you've already created. You're going to 
+have to be resourceful to figure out how to do this. However, you may continue to use the + 
+operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 */
@@ -136,5 +150,6 @@ export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and 
+// your laptop. 
 // You're done! Submit the link to the repo following the instructions in Canvas.
