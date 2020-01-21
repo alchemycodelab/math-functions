17abaa3fb0d49fdc8aac15e66a6a75b28ec689ba
Maximilian Moga <maximilianmoga@gmail.com>
2020-01-20 17:43:50 -0800

    wrote function 1

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,8 +12,9 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
-}
+let answer = a + b;
+let text = `The sum of ${a} + ${b} is ${answer}.`;
+return [answer, text];
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
@@ -26,6 +27,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
+    
 
 }
 
