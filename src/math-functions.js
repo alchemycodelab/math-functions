cc9f0b71b7cc9634541b617ee006717dd565311f
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-03 14:26:36 -0700

    sum solution is working and passing test

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,9 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const sumOfNumbers = a + b; 
+    const stringOfSum = `The sum of ${a} and ${b} is ${sumOfNumbers}.`;
+    return [sumOfNumbers, stringOfSum];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
