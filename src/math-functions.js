af5f4d218851f0b4356b47bb59ddcc2c4a961d9d
Daniel Bennington <dbennington125@gmail.com>
2020-04-03 17:27:52 -0700

    sum array test passed

--- src/math-functions.js
+++ src/math-functions.js
@@ -85,7 +85,11 @@ to use the + operator for string concatenation.
 */
 
 export function sumArrayWithThreeNumbers(sumArr) {
-    cost 
+    const sumA = sum(sumArr[0], sumArr[1]);
+    const sumB = sum(sumA[0], sumArr[2])[0];
+     
+    return [sumB, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumB} is their sum.`];
+
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
