454fd88967d7bccae0252c5844bf911c947d41a3
Michelle Stermitz <stermitzm@gmail.com>
2020-07-10 11:58:48 -0700

    Added multiplyFunction

--- src/math-functions.js
+++ src/math-functions.js
@@ -31,6 +31,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
+    const multipliedNums = a * b;
+    const resultString = `The product of ${a} and ${b} is ${multipliedNums}.`;
+    let multiplyArray = [multipliedNums, resultString];
+
+    return multiplyArray;
 
 }
 
