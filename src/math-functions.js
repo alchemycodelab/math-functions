70b49fdc1b5928ce308c3829518c72ff21884e6c
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-10 11:36:36 -0700

    initial commit before creating branch

--- src/math-functions.js
+++ src/math-functions.js
@@ -122,3 +122,5 @@ export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // You're done! Submit the link to the repo following the instructions in Canvas.
+
+//small change for intial commit before branch
