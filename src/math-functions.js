8ec29ca3c3a8cc709171313e5d64171a8e1188b1
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-07-10 12:10:05 -0700

    changed my first-test since I did it wrong

--- src/math-functions.js
+++ src/math-functions.js
@@ -13,7 +13,9 @@ example and uses the values that were input into the function:
 
 export function sum(a, b) {
    
-    return [11, 'The sum of 4 and 7 is 11.'];
+    const result = a + b;
+    const sumString = 'The product ${a} and {b} is {result,}.';
+    return [result, sumString];
     
 }
 
@@ -29,6 +31,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 export function multiply(a, b) {
 
+    
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -53,6 +56,7 @@ how to do this. However, you may continue to use the + operator for string conca
 */
 
 export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
+
 
 }
 
