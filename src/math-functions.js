a6f053058a677dd91bd75680668a647fc3fbfe2c
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-10-02 14:45:40 -0700

    second function passing

--- src/math-functions.js
+++ src/math-functions.js
@@ -27,7 +27,8 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    const multResult = (Number(a * b));
+    return [multResult, `The product of ${a} and ${b} is ${multResult}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
