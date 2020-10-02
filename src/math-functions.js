3c70872a1cebda215b05dc4540c39e5729373935
JustinMartin7x <justin.martin7x@gmail.com>
2020-10-02 16:46:53 -0700

    fixed Eslint issues

--- src/math-functions.js
+++ src/math-functions.js
@@ -87,7 +87,7 @@ to use the + operator for string concatenation.
 export function sumArrayWithThreeNumbers(sumArr) {
     const sumOfTwo = sum(sumArr[0], sumArr[1]);
     const sumOfThree = sum(sumOfTwo[0], sumArr[2]);
-    return [`${sumOfThree[0]}`, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumOfThree[0]} is their sum.`]
+    return [`${sumOfThree[0]}`, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumOfThree[0]} is their sum.`];
     
 
 }
