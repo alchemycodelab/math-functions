8020e994b8a1d28c6053636210d86efa83bddfe7
sarah-svg <kjskillen@gmail.com>
2020-10-02 14:12:04 -0700

    add

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,12 @@
 import './math-functions.test.js';
+ //let sum = [4, 7,]
+
+
+export function add(someNumber) {
+    let add = 0;
+    for(let i = 0; i < someNumber.length; i++) {
+        const someNumber = someNumber[i];
+        add = add += someNumber;
+    }
+    return add;
+}
