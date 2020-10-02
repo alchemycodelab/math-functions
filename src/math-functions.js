07e05d61e42538b896e505e19b158c4f4a08f5d7
kevinfiero <kjf46@cornell.edu>
2020-10-02 14:05:57 -0700

    second branch

--- src/math-functions.js
+++ src/math-functions.js
@@ -32,6 +32,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 export function multiply(a, b) {
 
+    const sumNum = a * b;
+    const sumString = `The product of ${a} and ${b} is ${sumNum}.`;
+    var sumArray = [sumNum, sumString];
+    return sumArray;
+
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
