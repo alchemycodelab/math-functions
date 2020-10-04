e6d1a3d10830bb5c9fb2233cbbc8dd2965505931
sarah-svg <kjskillen@gmail.com>
2020-10-04 11:44:18 -0700

    fifth test

--- test/tests.js
+++ test/tests.js
@@ -1,20 +1,2 @@
-import './math-functions.test.js';
- //let sum = [4, 7,]
+import { } from '../src/math-functions.js';
 
-
-export function add() {
-    let add = 0;
-    for(let i = 0; i < add.length; i++) {
-        const someNumber = someNumber[i];
-        add = add + someNumber;
-    }
-    return add;
-}
-export function multiply() {
-    let add = 0;
-    for(let i = 0; i < add.length; i++) {
-        const someNumber = someNumber[i];
-        add = add + someNumber;
-    }
-    return multiply;
-}
