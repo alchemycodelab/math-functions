23bcf9487cfc8dab2fd3d3323a374c9b1ec3ecca
Rhone Lachner <fernandclay@gmail.com>
2020-10-04 20:45:13 -0700

    gut push attempt 3 summult

--- src/math-functions.js
+++ src/math-functions.js
@@ -37,6 +37,7 @@ export function multiply(a, b) {
     
     return [multResult, string];
 }
+
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
 
