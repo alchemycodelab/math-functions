de8a6eafe775923e5bdcd73d16f61175d07f26ac
Rhone Lachner <fernandclay@gmail.com>
2020-10-04 20:40:55 -0700

    added mult funct test OK

--- src/math-functions.js
+++ src/math-functions.js
@@ -17,7 +17,6 @@ export function sum(a, b) {
     
     return [sumResult, string];
 }
-
 /*
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
