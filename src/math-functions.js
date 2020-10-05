b26b691e41ef73c728c330ce9fe4e436183930c0
Katelinn Jones <katiejonesyo@gmail.com>
2020-10-04 18:29:38 -0700

    number4

--- src/math-functions.js
+++ src/math-functions.js
@@ -60,8 +60,19 @@ how to do this. However, you may continue to use the + operator for string conca
 */
 
 export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
+    const sumOne = sum(a, b)[0];
+    const sumTwo = sum(sumOne, c)[0];
 
-}
+    const productOne = multiply(a, b)[0];
+    const productTwo = multiple(productOne, c)[0];
+
+    const sumString = '${a} and ${b} and ${c} sum to ${sumTwo}.';
+    const productString = 'The product of ${a} and {b} adn ${c} is ${productTwo}.';
+
+    const sumArray = [sumTwo, productTwo, sumString, productString];
+    
+    return sumArray;
+};
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
@@ -82,7 +93,13 @@ to use the + operator for string concatenation.
 */
 
 export function sumArrayWithThreeNumbers(sumArr) {
+    const sumOne = sum(sumArr[0], sumArr[1][0]);
+    const sumTwo = sum(sumOne, sumArr[2])[0];
 
+    const sumString = 'The numbers ${sumArr[0]}, ${sumArr[1]}, ${sumArr[2]} have a sum of ${sumTwo}.';
+    const sumArray = [sumTwo, sumString];
+
+    return sumArray;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -126,18 +143,7 @@ This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-    const sumOne = sum(a, b)[0];
-    const sumTwo = sum(sumOne, c)[0];
-
-    const productOne = multiply(a, b)[0];
-    const productTwo = multiple(productOne, c)[0];
-
-    const sumString = '${a} and ${b} and ${c} sum to ${sumTwo}.';
-    const productString = 'The product of ${a} and {b} adn ${c} is ${productTwo}.';
-
-    const sumArray = [sumTwo, productTwo, sumString, productString];
     
-    return sumArray;
 };
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
