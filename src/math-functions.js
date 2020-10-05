2e88403768eefa50bcaf5102774b4a912c104299
sarah-svg <kjskillen@gmail.com>
2020-10-05 10:43:47 -0700

    fixed the issues

--- src/math-functions.js
+++ src/math-functions.js
@@ -36,7 +36,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 */
 
-    export function multiply(a, b) {
+export function multiply(a, b) {
     let returnNumber = a * b;
     const returnString = 'The product of ' + a + ' and ' + b + ' is ' + returnNumber + '.';
     return [returnNumber, returnString];
--- test/math-functions.test.js
+++ test/math-functions.test.js
@@ -1,34 +1,30 @@
 import { 
-    sum,
-    multiply,
-    sumAndMultiplyThreeNumbers,
-   sumArrayWithThreeNumbers, 
+    sum, 
+    multiply, 
     sumAndMultiplyThreeNumbers, 
     sumArrayWithThreeNumbers, 
-
+    multiplyArrayWithThreeNumbers, 
+    multiplyAnyArray
 } from '../src/math-functions.js';
 
 const test = QUnit.test;
 
 /* DO NOT MODIFY THIS FILE */
 
-//1
 test('sum', (assert) => {
-  
     const result = sum(4, 7);
 
-
     assert.equal(result[0], 11);
-   assert.equal(result[1], 'The sum of 4 and 7 is 11.');
+    assert.equal(result[1], 'The sum of 4 and 7 is 11.');
 });
-//2
+
 test('multiply', (assert) => {
     const result = multiply(5, 9);
 
     assert.equal(result[0], 45);
     assert.equal(result[1], 'The product of 5 and 9 is 45.');
 });
-//3
+
 test('sumAndMultiplyThreeNumbers', (assert) => {
     const result = sumAndMultiplyThreeNumbers(4, 7, 5);
 
@@ -37,11 +33,6 @@ test('sumAndMultiplyThreeNumbers', (assert) => {
     assert.equal(result[2], '4 and 7 and 5 sum to 16.');
     assert.equal(result[3], 'The product of 4 and 7 and 5 is 140.');
 });
-//4
-test('sumArrayWithThreeNumbers', (assert) => {
-    const result = sumArrayWithThreeNumbers([2, 3, 4]);
-
-
 
 test('sumArrayWithThreeNumbers', (assert) => {
     const result = sumArrayWithThreeNumbers([2, 3, 4]);
@@ -50,16 +41,13 @@ test('sumArrayWithThreeNumbers', (assert) => {
     assert.equal(result[1], '2,3,4 was passed in as an array of numbers, and 9 is their sum.');
 });
 
-
-
-//5
 test('multiplyArrayWithThreeNumbers', (assert) => {
     const result = multiplyArrayWithThreeNumbers([2, 3, 4]);
 
     assert.equal(result[0], 24);
     assert.equal(result[1], 'The numbers 2,3,4 have a product of 24.');
 });
-//6
+
 test('multiplyAnyArray', (assert) => {
     const result = multiplyAnyArray([1, 2, 3, 4, 5]);
 
