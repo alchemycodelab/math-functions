3c694973f8f8d25a15d44a5f6634eb3df4c9f6d7
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-15 14:40:45 -0800

    create and test working function for problem 4

--- src/math-functions.js
+++ src/math-functions.js
@@ -14,24 +14,7 @@ export function multiply(a, b) {
     return [product, `The product of ${a} and ${b} is ${product}.`];
 }
 
-/////////////////////////////////////
-/* Problem 3
-Write a function called sumAndMultiplyThreeNumbers() that takes in three 
-numbers as separate arguments and returns 
-an array where the first element is the sum of those three numbers, 
-the second element is the product of those three numbers,  
-and the third and fourth elements are strings that EXACTLY follow this example and 
-use the values that were input into the function:
-
-Third element: "4 and 7 and 5 sum to 16."
-Fourth element: "The product of 4 and 7 and 5 is 140."
-
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. 
-To do addition, use your sum() function, and to do multiplication, use your multiply() 
-function that you've already created. You're going to have to be resourceful to figure out 
-how to do this. However, you may continue to use the + operator for string concatenation.
-
-*/
+// X Problem 3
 
 export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
     let sum1 = sum(a, b)[0];
@@ -41,26 +24,15 @@ export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
     return [addend, product, `${a} and ${b} and ${c} sum to ${addend}.`, `The product of ${a} and ${b} and ${c} is ${product}.`];
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
-// Don't forget to create a new branch for your work on the next question!
-
-/////////////////////////////////////
-/* Problem 4
-Write a function called sumArrayWithThreeNumbers() that takes in an array of numbers 
-as its single argument and then returns an array where the first element is the sum 
-of the numbers in the array, and the second element is a string that EXACTLY follows 
-this example and uses the values that were input into the function:
-
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. 
-To do addition, use your sum() function that you've already created. You're going to 
-have to be resourceful to figure out how to do this. However, you may continue 
-to use the + operator for string concatenation.
-*/
+// X Problem 4
 
 export function sumArrayWithThreeNumbers(sumArr) {
-
+    let addend = 0;
+    let numString = sumArr.join();
+    for (let num of sumArr) {
+        addend = sum(addend, num)[0];
+    }
+    return [addend, `${numString} was passed in as an array of numbers, and ${addend} is their sum.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
