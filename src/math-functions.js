b9e09a96d7eeef0956f544768d09286804d344f4
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-15 14:19:02 -0800

    adds the multipy function

--- src/math-functions.js
+++ src/math-functions.js
@@ -27,7 +27,8 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    const numbers = a * b;
+    return [numbers, `The product of ${a} and ${b} is ${a * b}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
