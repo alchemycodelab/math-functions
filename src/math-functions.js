4d7cfed1dfe5c21af6bc316f1ae58817a69d248d
Dominique Sakrisson <domsakrisson@gmailcom>
2021-01-15 13:41:05 -0800

    initial commit mult-branch

--- src/math-functions.js
+++ src/math-functions.js
@@ -31,6 +31,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 export function multiply(a, b) {
 
+    
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
