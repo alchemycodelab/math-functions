de651bbf1575a930699ffc601e028c232a7e91a6
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-15 15:15:58 -0800

    create and test stretch problem

--- src/math-functions.js
+++ src/math-functions.js
@@ -35,7 +35,7 @@ export function sumArrayWithThreeNumbers(sumArr) {
     return [addend, `${numString} was passed in as an array of numbers, and ${addend} is their sum.`];
 }
 
-// Problem 5
+// X Problem 5
 
 export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
     let product = 1;
@@ -46,26 +46,13 @@ export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
     return [product, `The numbers ${numString} have a product of ${product}.`];
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
-
-// You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
-
-// Don't forget to create a new branch for your work on the next question, if you attempt it.
-
-/////////////////////////////////////
-/* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
-
-"The numbers 1,2,3,4,5 have a product of 120."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-
-This function should be dynamic, accepting an array of any length.
-*/
+// X Problem 6 stretch
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    let product = 1;
+    let numString = dynamicArray.join();
+    for (let num of dynamicArray) {
+        product = multiply(product, num)[0];
+    }
+    return [product, `The numbers ${numString} have a product of ${product}.`];
 }
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
-// You're done! Submit the link to the repo following the instructions in Canvas.
