02fc0b9fd4f1c89c4099a8eded42c1035d5baa82
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-01-15 17:51:07 -0500

    reread instructions and completed function the correct way, test passing

--- src/math-functions.js
+++ src/math-functions.js
@@ -14,6 +14,7 @@ example and uses the values that were input into the function:
 export function sum(a, b) {
     const addedNumbers = (a + b);
     return [addedNumbers, `The sum of ${a} and ${b} is ${addedNumbers}.`];
+
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -22,7 +23,7 @@ export function sum(a, b) {
 /////////////////////////////////////
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
+ 
 "The product of 5 and 9 is 45."
 */
 
@@ -43,10 +44,10 @@ an array where the first element is the sum of those three numbers,
 the second element is the product of those three numbers,  
 and the third and fourth elements are strings that EXACTLY follow this example and 
 use the values that were input into the function:
-
+ 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
-
+ 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. 
 To do addition, use your sum() function, and to do multiplication, use your multiply() 
 function that you've already created. You're going to have to be resourceful to figure out 
@@ -54,8 +55,10 @@ how to do this. However, you may continue to use the + operator for string conca
 */
 
 export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
-    const sumOfThree = (a + b + c);
-    const productOfThree = (a * b * c);
+    const sumOnly = sum(a, b)[0];
+    const sumOfThree = sum(sumOnly, c)[0];
+    const productOnly = multiply(a, b)[0];
+    const productOfThree = multiply(productOnly, c)[0];
 
     return [sumOfThree,
         productOfThree,
@@ -73,9 +76,9 @@ Write a function called sumArrayWithThreeNumbers() that takes in an array of num
 as its single argument and then returns an array where the first element is the sum 
 of the numbers in the array, and the second element is a string that EXACTLY follows 
 this example and uses the values that were input into the function:
-
+ 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
+ 
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. 
 To do addition, use your sum() function that you've already created. You're going to 
 have to be resourceful to figure out how to do this. However, you may continue 
@@ -95,9 +98,9 @@ Write a function called multiplyArrayWithThreeNumbers() that takes an array of n
 as its argument and returns an array whose first element is the product of those numbers, 
 and the second element is a string that EXACTLY follows this example and uses the values 
 that were input into the function:
-
+ 
 "The numbers 2,3,4 have a product of 24."
-
+ 
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. 
 To do multiplication, use your multiply() function that you've already created. 
 You're going to have to be resourceful to figure out how to do this. 
@@ -118,11 +121,11 @@ export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
 Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
-
+ 
 "The numbers 1,2,3,4,5 have a product of 120."
-
+ 
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-
+ 
 This function should be dynamic, accepting an array of any length.
 */
 
