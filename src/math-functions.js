eb1e713968e93f26c14f777e86a92649da0cbe4a
Soraya Benson <soraya.benson@gmail.com>
2021-01-15 14:58:53 -0800

    changes made to pass github check again

--- src/math-functions.js
+++ src/math-functions.js
@@ -117,7 +117,7 @@ you may continue to use the + operator for string concatenation.
 
 /*export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
 
-}
+}*/
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
@@ -138,7 +138,7 @@ This function should be dynamic, accepting an array of any length.
 
 /*export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
-}
+}*/
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 // You're done! Submit the link to the repo following the instructions in Canvas.
