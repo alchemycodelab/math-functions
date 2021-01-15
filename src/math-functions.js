7928726ef316d77659680c97fbeeb46ec8a0cbaa
stephen-leisy <stleisy@gmail.com>
2021-01-15 14:36:17 -0800

    second problem solved with passing test.

--- src/math-functions.js
+++ src/math-functions.js
@@ -27,9 +27,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 */
 
-// export function multiply(a, b) {
-
-// }
+export function multiply(a, b) {
+    return [a * b, `The product of ${5} and ${9} is ${5 * 9}.`];
+}
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
