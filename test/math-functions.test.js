05da15bcac00c12bb7ac77c8ceb0cb70a593a9b9
Cherno Badjan <badjancherno@gmail.com>
2021-01-15 17:53:36 -0800

    stretch

--- test/math-functions.test.js
+++ test/math-functions.test.js
@@ -49,7 +49,7 @@ test('multiplyArrayWithThreeNumbers', (assert) => {
     assert.equal(result[1], 'The numbers 2,3,4 have a product of 24.');
 });
 
-skip('multiplyAnyArray', (assert) => {
+test('multiplyAnyArray', (assert) => {
     const result = multiplyAnyArray([1, 2, 3, 4, 5]);
 
     assert.equal(result[0], 120);
