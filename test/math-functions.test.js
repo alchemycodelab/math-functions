6575f1eeb5279f1b3d312ea6e14dd12e635c3830
taylorreichner <taylorreichner@yahoo.com>
2021-01-15 16:06:50 -0800

    finsished third test

--- test/math-functions.test.js
+++ test/math-functions.test.js
@@ -1,7 +1,7 @@
 import { 
     sum, 
     multiply, 
-    //sumAndMultiplyThreeNumbers, 
+    sumAndMultiplyThreeNumbers, 
     //sumArrayWithThreeNumbers, 
     //multiplyArrayWithThreeNumbers, 
     //multiplyAnyArray
@@ -25,7 +25,7 @@ test('multiply', (assert) => {
     assert.equal(result[1], 'The product of 5 and 9 is 45.');
 });
 
-/*test('sumAndMultiplyThreeNumbers', (assert) => {
+test('sumAndMultiplyThreeNumbers', (assert) => {
     const result = sumAndMultiplyThreeNumbers(4, 7, 5);
 
     assert.equal(result[0], 16);
@@ -34,7 +34,7 @@ test('multiply', (assert) => {
     assert.equal(result[3], 'The product of 4 and 7 and 5 is 140.');
 });
 
-test('sumArrayWithThreeNumbers', (assert) => {
+/*test('sumArrayWithThreeNumbers', (assert) => {
     const result = sumArrayWithThreeNumbers([2, 3, 4]);
 
     assert.equal(result[0], 9);
