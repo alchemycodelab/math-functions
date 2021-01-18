959679812a5c32530773344c0f53c5cb45fde11a
Nicole Martin <n.martinpdx@gmail.com>
2021-01-18 15:08:46 -0800

    problem 2 solved

--- src/math-functions.js
+++ src/math-functions.js
@@ -29,7 +29,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    const multiSum = a * b;
+    const sumString = `The product of ${a} and ${b} is ${a * b}.`;
+    return [multiSum, sumString];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
