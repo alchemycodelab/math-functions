b31d3b9ccf24bfcfc1719b940f489b27611c014c
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-29 14:33:54 -0700

    fixed Problem 3 to meet instruction standards

--- src/math-functions.js
+++ src/math-functions.js
@@ -58,9 +58,11 @@ how to do this. However, you may continue to use the + operator for string conca
 */
 
 export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
-    const addSum = a+b+c;
-    const multSum = a*b*c;
-    const reqArray = [addSum, multSum, `${a} and ${b} and ${c} sum to ${addSum}.`, `The product of ${a} and ${b} and ${c} is ${multSum}.`];
+    const add = sum(a, b);
+    const addSum = sum(add[0], c);
+    const mult = multiply(a, b);
+    const multSum = multiply(mult[0], c);
+    const reqArray = [addSum[0], multSum[0], `${a} and ${b} and ${c} sum to ${addSum[0]}.`, `The product of ${a} and ${b} and ${c} is ${multSum[0]}.`];
     return reqArray;
 }
 
@@ -81,9 +83,18 @@ To do addition, use your sum() function that you've already created. You're goin
 have to be resourceful to figure out how to do this. However, you may continue 
 to use the + operator for string concatenation.
 */
+// const sampleArray[a, b, c];
+
 
 export function sumArrayWithThreeNumbers(sumArr) {
+    let arrayTotal = 0;
+    for (let num of sumArr) {
+        arrayTotal = sum(arrayTotal, num);
+        console.log(arrayTotal);
+    }
 
+    const reqArray = [arrayTotal, `${sumArr} was passed in as an array of numbers, and ${arrayTotal[0]} is their sum.`];
+    return reqArray;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
