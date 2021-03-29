69866bd9ff516aea0763190e34cb6c8b1e3fc7d2
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-03-29 13:59:13 -0700

    sum branch

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,9 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const result = a + b;
+    const returnString = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
+    return [result, returnString];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
