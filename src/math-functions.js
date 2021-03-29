00dcc473dee60d3ff393554ce1981dc43d2cf1b0
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-29 16:50:10 -0400

    create multiplication function

--- src/math-functions.js
+++ src/math-functions.js
@@ -38,6 +38,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 export function multiply(a, b) {
 
+    const returnArray = [];
+
+    const productResult = a * b;
+
+    const productString = `The product of ${a} and ${b} is ${productResult}.`;
+
+    returnArray.push(productResult);
+    returnArray.push(productString);
+
+    return returnArray;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
