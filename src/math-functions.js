6d83096ff28856e5a45dc8b7efe1a40e72c239bb
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-03-29 19:32:59 -0400

    multiply any array func

--- src/math-functions.js
+++ src/math-functions.js
@@ -159,17 +159,27 @@ export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length 
+as its argument and returns an array whose first element is the product of those numbers, 
+and the second element is a string that EXACTLY follows this example and concatenates a message
+ using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do 
+multiplication, use your multiply() function that you've already created. You're going to 
+have to be resourceful to figure out how to do this. However, you may continue to use the + 
+operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    let finalProduct = 1;
+    for(let num of dynamicArray)
+        finalProduct = multiply(num, finalProduct)[0];
     
+    return [finalProduct, `The numbers ${dynamicArray} have a product of ${finalProduct}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
