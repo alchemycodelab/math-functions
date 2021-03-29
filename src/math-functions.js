bb202aeb337bad300d4b1f1a353bf7eb7b408353
austinxduong <axduong2@gmail.com>
2021-03-29 15:31:11 -0700

    added multiplication

--- src/math-functions.js
+++ src/math-functions.js
@@ -27,6 +27,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
+    return [a * b, `The sum of ${a} and ${b} is ${a * b}.`];
 
 }
 
