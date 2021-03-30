62f5f231e10d5c37edcd82175804d57f53a30f58
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-03-29 17:47:28 -0700

    add refactored stretch goal

--- src/math-functions.js
+++ src/math-functions.js
@@ -149,7 +149,11 @@ This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    let product = 1;
+    for (let i of dynamicArray) {
+        product = multiply(product, i)[0];
+    }
+    return [product, `The numbers ${dynamicArray} have a product of ${product}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
