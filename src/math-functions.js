ec02a681c6c6ef729abc6b6952026b8258c18798
DJ Drakos <dianajodrakos@gmail.com>
2021-05-06 14:32:01 -0700

    sum function test passed

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,6 +12,11 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
+    return [
+        a + b,
+        `The sum of ${a} and ${b} is ${a + b}.`
+    ];
+
 
 }
 
