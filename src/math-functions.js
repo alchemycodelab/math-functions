e02728084ce18b67398017ae98ff2e24af3df6fc
chenerychen <zhaocsnh@gmail.com>
2021-05-06 18:16:23 -0400

    added sumAndMultiplyThreeNumbers

--- src/math-functions.js
+++ src/math-functions.js
@@ -15,6 +15,8 @@ export function sum(a, b) {
     return ([a + b, `The sum of ${a} and ${b} is ${a + b}.`]);
 }
 
+// [2, `The sum of ${a} and ${b} is ${a + b}.`] ]
+
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
 
@@ -51,8 +53,9 @@ how to do this. However, you may continue to use the + operator for string conca
 */
 
 export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
-
+    return [`${sum(sum(a, b)[0], c)[0]}`, `${multiply(multiply(a, b)[0], c)[0]}`, `${a} and ${b} and ${c} sum to ${sum(sum(a, b)[0], c)[0]}.`, `The product of ${a} and ${b} and ${c} is ${multiply(multiply(a, b)[0], c)[0]}.`]; 
 }
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
