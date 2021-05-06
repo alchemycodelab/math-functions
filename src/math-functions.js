87621921e348e99f27acbf35ed65b2e8d322bacd
DJ Drakos <dianajodrakos@gmail.com>
2021-05-06 14:55:16 -0700

    multiply function passes test

--- src/math-functions.js
+++ src/math-functions.js
@@ -31,9 +31,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    return [
+        a * b,
+        `The product of ${a} and ${b} is ${a * b}.`
+    ];
 }
-
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
 
