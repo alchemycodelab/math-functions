ec42bbe0d9740f782e33f976c7fc2f9e0924dab6
maddenlockin <maddenlockin@gmail.com>
2021-05-07 09:45:55 -0700

    sum function working

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,10 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const sum = a + b;
+    return [
+        sum,
+        `The sum of ${a} and ${b} is ${sum}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -71,10 +74,17 @@ To do addition, use your sum() function that you've already created. You're goin
 have to be resourceful to figure out how to do this. However, you may continue 
 to use the + operator for string concatenation.
 */
+    export function sumArrayWithThreeNumbers(summArr) {
+    //     let sumTotal = 0;
+    //     for (let num of sumArr) {
+    //         sumTotal = sum(sumTotal, num)[0];
+    //     }
+    //     return [
+    //         sumTotal,
+    //         `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumTotal} is their sum.`
+    //     ]
+    }
 
-export function sumArrayWithThreeNumbers(sumArr) {
-
-}
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
