b841244d13d902581a93e15c028430ad1096a5a5
Peter <pete225848@gmail.com>
2021-05-07 17:44:27 -0700

    fixed -.-

--- src/math-functions.js
+++ src/math-functions.js
@@ -134,6 +134,9 @@ This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    return [120,
+    "The numbers 1,2,3,4,5 have a product of 120."
+    ]
     
 
 
