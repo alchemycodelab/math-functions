fe5fab38c4712fc1b1d954a15b2d3dca303ef7d9
Peter <pete225848@gmail.com>
2021-05-13 11:47:26 -0700

    completed sumAndMult3N function

--- src/math-functions.js
+++ src/math-functions.js
@@ -59,6 +59,12 @@ how to do this. However, you may continue to use the + operator for string conca
 */
 
 export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
+    return [
+        sum(sum(a, b)[0], c)[0],
+        multiply(multiply(a, b)[0], c)[0],
+        `${a} and ${b} and ${c} sum to ${sum(sum(a, b)[0], c)[0]}.`,
+        `The product of ${a} and ${b} and ${c} is ${multiply(multiply(a, b)[0], c)[0]}.`
+    ];
 
 }
 
