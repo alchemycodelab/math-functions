90db056e350f0fc4d1aaa03d1fc78b14ae400a58
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-07-12 09:54:44 -0700

    PR for math problem 1

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,6 +12,8 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
+    const sum = a + b;
+    return [sum, `The sum of ${a} and ${b} is ${sum}.`];
 
 }
 
